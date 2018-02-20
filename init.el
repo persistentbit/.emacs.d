e75fc0560c9c993d84ff4a4e34b78d38850a2fc4
Peter Muys <peter@petermuys.com>
2018-02-20 13:15:35 +0100

    init

--- init.el
+++ init.el
@@ -12,6 +12,34 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;;(package-initialize)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+   t))
+
+;; Remove startup screen
+;;
+(setq inhibit-startup-screen t)
+
+;;  Init UI Themes load path
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+;; Load the zenburn theme
+;; (load-theme 'zenburn t)
+
+;; Load the monokai theme
+(load-theme 'monokai t)
+
+
 ;; Turn on Text Mode and auto-fill mode automatically
 (setq default-major-mode 'text-mode)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
