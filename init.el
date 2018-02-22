03612cda3d6098223906dd2bd84cf6197c8aa463
Peter Muys <peter.muys@kbc.be>
2018-02-22 13:59:20 +0100

    Added use-package and ace-window

--- init.el
+++ init.el
@@ -1,23 +1,8 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode t)
- '(cua-mode t nil (cua-base))
- '(package-selected-packages (quote (magit)))
- '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;;(package-initialize)
+(package-initialize)
 
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -27,6 +12,33 @@
    '("melpa" . "http://melpa.milkbox.net/packages/")
    t))
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+;;(require 'diminish)
+(require 'bind-key)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+ '(package-selected-packages
+   (quote
+    (ace-window dracula-theme projectile use-package magit)))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; Remove startup screen
 ;;
 (setq inhibit-startup-screen t)
@@ -76,3 +88,36 @@ This command switches to browser."
     (browse-url (concat "http://en.wikipedia.org/wiki/" word))
     ;; (eww myUrl) ; emacs's own browser
     ))
+
+;;
+;; Projectile settings
+;;
+(setq projectile-indexing-method 'native)
+(setq projectile-enable-caching t)
+
+;;
+;; reformat xml
+;;
+
+(require 'sgml-mode)
+
+(defun my/reformat-xml ()
+  (interactive)
+  (save-excursion
+    (sgml-pretty-print (point-min) (point-max))
+    (indent-region (point-min) (point-max))))
+;;
+;; ace-window 
+;; Set the swith key to M-o and make the home row the access keys
+;;(global-set-key (kbd "M-o") 'ace-window)
+;;(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(use-package ace-window
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (global-set-key (kbd "M-o") 'ace-window)
+    (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+     ;;more info at https://github.com/abo-abo/ace-window
+    )
+  )
